5a6853f45a4e57db92f6a39d6c07649f5f90201b
sk-vojik <scott.vojik@gmail.com>
2019-06-16 14:43:17 -0700

    updated cors

--- api/middlware/middleware.js
+++ api/middlware/middleware.js
@@ -4,7 +4,13 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const corsOptions = {
+  credentials: true,
+}
+
+
 module.exports = server => {
+  server.use(cors(corsOptions));
   server.use(helmet());
   server.use(express.json());
   server.use(morgan("dev"));
